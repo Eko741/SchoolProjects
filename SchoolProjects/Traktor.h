96bc676373e09f0604e37e6441f1d89ae2ff2614
Eko741 <erik@lindaroger.com>
2022-03-16 13:08:46 +0100

    Help

--- SchoolProjects/Traktor.h
+++ SchoolProjects/Traktor.h
@@ -3,7 +3,7 @@
 
 class Traktor : public Fordon {
 public:
-	Traktor(int BWS = 20, int FWS = 10, int TW = 0, bool hasTrailer = false, double c = 0.1, int w = 10000, int n = 4, int r = 0, int g = 0, int b = 0) : Fordon(c, w, n, r, g, b), backWheelSize(BWS), frontWheelSize(FWS), hasTrailer(HS), trailerWeight(TW) {}
+	Traktor(int BWS = 20, int FWS = 10, int TW = 0, bool HT = false, double c = 0.1, int w = 10000, int n = 4, int r = 0, int g = 0, int b = 0) : Fordon(c, w, n, r, g, b), backWheelSize(BWS), frontWheelSize(FWS), hasTrailer(HT), trailerWeight(TW) {}
 
 	int BWS() { return backWheelSize; }
 	bool move(int distance);
